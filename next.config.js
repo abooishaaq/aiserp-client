0ce3b82e996b88f1e5728a76abfaace01bb4c718
Malik Ammar Faisal <binfaisal.ammar@gmail.com>
2022-08-12 13:56:34 +0530

    re add pwa

--- next.config.js
+++ next.config.js
@@ -1,12 +1,21 @@
 const withPWA = require("next-pwa");
 const runtimeCaching = require("next-pwa/cache");
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-    enabled: process.env.ANALYZE === 'true',
-  })
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+    enabled: process.env.ANALYZE === "true",
+});
 
 /** @type {import('next').NextConfig} */
-const nextConfig = withBundleAnalyzer({
-    swcMinify: true,
-});
+const nextConfig = withBundleAnalyzer(
+    withPWA({
+        swcMinify: true,
+        pwa: {
+            dest: "public",
+            register: false,
+            skipWaiting: true,
+            runtimeCaching,
+            disable: process.env.NODE_ENV === "development",
+        },
+    })
+);
 
 module.exports = nextConfig;

ec0cd498a9933dc65375dc5fd98c1778b25104a7
Malik Ammar Faisal <binfaisal.ammar@gmail.com>
2022-08-16 08:57:26 +0530

    update container classes

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -8,7 +8,7 @@ const Dashboard = () => {
             <Head>
                 <title>Admin Dashboard</title>
             </Head>
-            <div className="container max-h-screen bg-beige/95 mx-auto max-w-4xl mg:max-w-5xl">
+            <div className="container h-screen bg-beige/95 max-h-screen max-w-3xl md:max-w-4xl mg:max-w-5xl overflow-hidden">
                 <h1 className="text-4xl font-semibold my-8">Welcome</h1>
             </div>
         </>
